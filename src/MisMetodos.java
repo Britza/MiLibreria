833fdac4fdc0e4fc4bae6834ad6618f6815a072e
britza <bzegarraflores@danielcastelao.org>
2021-02-12 09:10:14 +0100

    Initial commit

---
+++ src/MisMetodos.java
@@ -0,0 +1,13 @@
+/**
+ * Contiene metodos utiles para usar en los proyectos
+ */
+public class MisMetodos {
+    /**
+     * Metodo que monta un saludo
+     * @param mensaje saludo que quiero mostrar
+     * @return cadena con el mensaje concatenado con otro String
+     */
+    public static String saludo(String mensaje){
+        return "Saludo: " + mensaje;
+    }
+}
