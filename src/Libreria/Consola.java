496b3d9995f796032acc67ca7918e001621e7314
britza <bzegarraflores@danielcastelao.org>
2021-03-02 10:41:29 +0100

    Package creado

---
+++ src/Libreria/Consola.java
@@ -0,0 +1,13 @@
+package Libreria;
+
+public class Consola {
+
+    private String msg;
+
+    public Consola(String msg) {
+        this.msg = msg;
+    }
+    public void visualizar(){
+        System.out.println("El mensaje se visualiza por consola.\n"+msg);
+    }
+}
